c24c434fc6c51eae6a5f36d35ea1d6cffc0a7b8b
vandanasriram <55282441+vandanasriram@users.noreply.github.com>
2019-10-20 16:29:50 -0400

    Update script.js
    Checkout items js update

--- script.js
+++ script.js
@@ -123,3 +123,188 @@ var imgtag;
 var secLangflag;
 
 
+function checkout() {
+    var checkoutFlag
+    if(checkoutItem.length > 0) {
+        checkoutFlag = confirm("Total No of Items:" + checkoutItem.length + "\n" + "Click Ok to proceed checkout");
+        if (checkoutFlag == true) {
+            removeFromCart(undefined, "success")
+        } else {
+            removeFromCart(undefined, "cancel")
+        }
+    }
+    else {
+        alert("Cart is empty. Please add to check out")
+    }
+    
+}
+
+
+function uservalidation() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var year = document.getElementById("birth-year").value;
+    var validEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var errFLag = false;
+    var adminFlag = false;
+    var infomsg = document.getElementById("info");
+    var message;
+    var currentYear = new Date().getFullYear();
+    infomsg.innerText = "";
+
+        if (name.length > 100) {
+            errFLag = true;
+            alert("enter name less than 100 characters");
+        }
+        
+        if (name !== "admin") {
+            if (Number(year) < 1900) {
+                errFLag = true;
+                alert("Year should be after 1900");
+            }
+            if (Number(year) > currentYear) {
+                errFLag = true;
+                alert("Year should not be after current year");
+            }
+            if (validEmail.test(email) === false) {
+                errFLag = true;
+                alert("Enter valid email id")
+            }
+            if (errFLag === false) {
+                if(!document.getElementById("login-page").classList.contains("display-none")){
+                    document.getElementById("login-page").classList.add("display-none")
+                }
+                message = name + " (" + email + ") " + "[" + ((currentYear - year) >= 18 ? "Adult" : "Child") + "]";
+                infomsg.innerText = message;
+                document.getElementById("library-items-details").classList.remove("display-none");
+                document.getElementById("library-checkout-details").classList.remove("display-none");
+            }
+        }
+        else {
+            if (name === "admin" && Number(year) === 1867) {
+                if(!document.getElementById("login-page").classList.contains("display-none")){
+                    document.getElementById("login-page").classList.add("display-none")
+                }
+                if (document.getElementById("admin-ops").classList.contains("display-none")) {
+                    document.getElementById("admin-ops").classList.remove("display-none");
+                    document.getElementById("bookdue").innerText = dueDatesObj.book;
+                    document.getElementById("cddue").innerText = dueDatesObj.cd;
+                    message = "Librarian";
+                    infomsg.innerText = message;
+                    if(document.getElementById("library-items-details").classList.contains("display-none")) {
+                        document.getElementById("library-items-details").classList.remove("display-none")
+                    }
+                }
+    
+            }
+            else {
+                if (!document.getElementById("admin-ops").classList.contains("display-none")) {
+                    document.getElementById("admin-ops").classList.add("display-none");
+                }
+                alert("enter valid details");
+                errFLag = true;
+            }
+        }
+    
+        if (errFLag === true) {
+            document.getElementById("library-items-details").classList.add("display-none");
+            document.getElementById("library-checkout-details").classList.add("display-none");
+        }
+    
+
+}
+
+function addItem() {
+    if (!document.getElementById("library-checkout-details").classList.contains("display-none")) {
+        document.getElementById("library-checkout-details").classList.add("display-none");
+    }
+    if (document.getElementById("library-items-details").classList.contains("display-none")) {
+        document.getElementById("library-items-details").classList.remove("display-none")
+    }
+    var addItemName = document.getElementById("item-name").value;
+    var addItemCategory = document.getElementById("category").value;
+    var addItemName2 = document.getElementById("item-name2").value;
+    var itemObj = { item_name: "", category: "" , imgUrl:"",item_name2:""};
+    itemObj.item_name = addItemName;
+    itemObj.category = addItemCategory;
+    itemObj.imgUrl = "img/"+addItemName+".jpg";
+    itemObj.item_name2 = addItemName2;
+    itemsAvailable.push(itemObj);
+    alert("item Added successfully" + "\n" + "See the updated list below");
+    deleteAvailNodes("#available-items");
+    loadAvailableItems(secLangflag);
+
+}
+function removeItem() {
+    if (!document.getElementById("library-checkout-details").classList.contains("display-none")) {
+        document.getElementById("library-checkout-details").classList.add("display-none");
+    }
+    if (document.getElementById("library-items-details").classList.contains("display-none")) {
+        document.getElementById("library-items-details").classList.remove("display-none")
+    }
+    var addItemName = document.getElementById("item-name").value;
+    var addItemCategory = document.getElementById("category").value;
+    var itemLen = itemsAvailable.length;
+    var removeFlag = false;
+    for (var i = 0; i < itemLen; i++) {
+        if ((addItemName === itemsAvailable[i].item_name || addItemName === itemsAvailable[i].item_name2) && addItemCategory === itemsAvailable[i].category) {
+            itemsAvailable.splice(i, 1);
+            removeFlag = true;
+            break;
+        }
+    }
+    if (removeFlag == false) {
+        alert("Item not found");
+    }
+    else {
+        alert("removed successfully" + "\n" + "See the updated list below");
+        deleteAvailNodes("#available-items");
+        loadAvailableItems(secLangflag);
+    }
+}
+
+function updateDueDate() {
+    if (!document.getElementById("library-checkout-details").classList.contains("display-none")) {
+        document.getElementById("library-checkout-details").classList.add("display-none");
+    }
+    if (document.getElementById("library-items-details").classList.contains("display-none")) {
+        document.getElementById("library-items-details").classList.remove("display-none")
+    }
+    var name = document.getElementById("name").value;
+    var year = document.getElementById("birth-year").value;
+    var dueItemCategory = document.getElementById("due-category").value;
+    var newDueday = document.getElementById("due-value").value;
+
+    if (name === "admin" && Number(year) === 1867) {
+        dueDatesObj[dueItemCategory] = Number(newDueday);
+    }
+    document.getElementById("bookdue").innerText = dueDatesObj.book;
+    document.getElementById("cddue").innerText = dueDatesObj.cd;
+    alert("Due date updated successfully" + "\n" + "See the updated list below");
+    deleteAvailNodes("#available-items");
+    loadAvailableItems(secLangflag);
+}
+
+function logoutAdmin() {
+    var name = document.getElementById("name").value;
+    var year = document.getElementById("birth-year").value;
+    if (name === "admin" && Number(year) === 1867) {
+        if (!document.getElementById("admin-ops").classList.contains("display-none")) {
+            document.getElementById("admin-ops").classList.add("display-none");
+            document.getElementById("name").value = "";
+            document.getElementById("birth-year").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("info").innerText ="";
+        }
+        if (!document.getElementById("library-checkout-details").classList.contains("display-none")) {
+            document.getElementById("library-checkout-details").classList.add("display-none");
+        }
+        if (!document.getElementById("library-items-details").classList.contains("display-none")) {
+            document.getElementById("library-items-details").classList.add("display-none");
+        }
+        if(document.getElementById("login-page").classList.contains("display-none")){
+            document.getElementById("login-page").classList.remove("display-none")
+        }
+        adminLogoutFlag = true;
+    }
+}
